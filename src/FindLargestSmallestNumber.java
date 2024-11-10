d4717b231c228d8035a0fd1484784065d5511fb8
Ashutosh kumar <ashutosh2003kumar@gmail.com>
2024-11-10 23:17:35 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/FindLargestSmallestNumber.java
@@ -0,0 +1,20 @@
+public class FindLargestSmallestNumber {
+    public static void main(String[] args) {
+        int[] arr={12,56,76,89,100,343,21,234};
+
+        int largest=arr[0];
+        int smallest=arr[0];
+
+        for (int i = 0; i <arr.length ; i++) {
+            if(arr[i]>largest){
+                largest=arr[i];
+            }
+            else if(arr[i]<smallest){
+                smallest=arr[i];
+            }
+        }
+        System.out.println("Smallest Number is :" +smallest);
+        System.out.println("Largest Number is : "+largest);
+    }
+}
+//Time Complexity of above program is o(n).
