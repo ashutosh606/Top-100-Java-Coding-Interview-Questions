9d8dd970d9a312feeacbdede2e1fed1875db49aa
Ashutosh kumar <ashutosh2003kumar@gmail.com>
2024-11-10 17:58:45 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/Q1_ReverseString/Using_StringBuffer.java
@@ -0,0 +1,11 @@
+package Q1_ReverseString;
+
+public class Using_StringBuffer {
+    public static void main(String[] args) {
+        String str="Ashutosh";
+        StringBuffer sb=new StringBuffer(str);
+
+        System.out.println("Reverse of the str"+sb.reverse());
+    }
+
+}
